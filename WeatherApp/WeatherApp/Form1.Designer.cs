21db6540a9121eca7bb4b9ac16279c875b0db5fa
Zoltán Tomcsik <tomzol313@gmail.com>
2020-12-08 16:13:34 +0100

    GetDatas()

--- WeatherApp/WeatherApp/Form1.Designer.cs
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -29,13 +29,43 @@ namespace WeatherApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(174, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- WeatherApp/WeatherApp/Form1.cs
+++ WeatherApp/WeatherApp/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace WeatherApp
 {
     public partial class Form1 : Form
     {
+        BindingList<string> datas = new BindingList<string>();
+
         public Form1()
         {
             InitializeComponent();
+
+            GetDatas();
+        }
+
+        private void GetDatas()
+        {
+            var myweather = new WeatherServiceReference.ndfdXMLPortTypeClient();
+
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Now;
+
+            decimal lat = 32.7452M;
+            decimal lng = -117.1979M;
+
+            var productT = WeatherServiceReference.productType.timeseries;
+            var unit = WeatherServiceReference.unitType.m;
+
+            var parameters = new WeatherServiceReference.weatherParametersType();
+            {
+                parameters.temp = true;
+            };
+
+            var data = myweather.NDFDgen(lat, lng, productT,
+                startDate, endDate, unit, parameters);
+
+            var xml = new XmlDocument();
+            xml.LoadXml(data);
+
+            foreach (XmlElement element in xml.GetElementsByTagName("value"))
+            {
+                datas.Add(element.InnerText);
+            }
+
+            listBox1.DataSource = datas;
         }
     }
 }
