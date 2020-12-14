c11fa5416b61f30e5da05d1f05e0c2709334ac49
Zoltán Tomcsik <tomzol313@gmail.com>
2020-12-14 17:53:15 +0100

    designer

--- WeatherApp/WeatherApp/Form1.Designer.cs
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -164,7 +164,7 @@ namespace WeatherApp
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(554, 17);
+            this.label3.Location = new System.Drawing.Point(553, 17);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 20);
             this.label3.TabIndex = 19;
