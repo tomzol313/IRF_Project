068ebe58881f15750c7546850391c721715cf3a7
Zoltán Tomcsik <tomzol313@gmail.com>
2020-12-14 15:30:30 +0100

    design1

--- WeatherApp/WeatherApp/Form1.Designer.cs
+++ WeatherApp/WeatherApp/Form1.Designer.cs
@@ -37,12 +37,15 @@ namespace WeatherApp
             this.button2 = new System.Windows.Forms.Button();
             this.mainPanel = new System.Windows.Forms.Panel();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 45);
+            this.listBox1.Location = new System.Drawing.Point(13, 71);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(180, 108);
             this.listBox1.TabIndex = 0;
@@ -50,7 +53,7 @@ namespace WeatherApp
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 19);
+            this.textBox1.Location = new System.Drawing.Point(13, 45);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(179, 20);
             this.textBox1.TabIndex = 1;
@@ -58,7 +61,8 @@ namespace WeatherApp
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(234, 160);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(234, 186);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 4;
@@ -72,7 +76,7 @@ namespace WeatherApp
             this.comboBox1.Items.AddRange(new object[] {
             "Celsius",
             "Fahrenheit"});
-            this.comboBox1.Location = new System.Drawing.Point(210, 19);
+            this.comboBox1.Location = new System.Drawing.Point(210, 45);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(152, 21);
             this.comboBox1.TabIndex = 9;
@@ -89,14 +93,15 @@ namespace WeatherApp
             "Csapadék valószínűsége",
             "Páratartalom",
             "Szélerősség"});
-            this.checkedListBox1.Location = new System.Drawing.Point(210, 45);
+            this.checkedListBox1.Location = new System.Drawing.Point(210, 71);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(152, 109);
             this.checkedListBox1.TabIndex = 10;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(434, 195);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(428, 221);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 29);
             this.button2.TabIndex = 12;
@@ -106,7 +111,7 @@ namespace WeatherApp
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(592, 8);
+            this.mainPanel.Location = new System.Drawing.Point(592, 34);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(200, 200);
             this.mainPanel.TabIndex = 13;
@@ -114,17 +119,51 @@ namespace WeatherApp
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(379, 19);
+            this.listView1.Location = new System.Drawing.Point(379, 45);
             this.listView1.Name = "listView1";
+            this.listView1.Scrollable = false;
             this.listView1.Size = new System.Drawing.Size(200, 170);
             this.listView1.TabIndex = 14;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(40, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Város keresése";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(204, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Megjelenítendő adatok";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(443, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Adatok";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 228);
+            this.ClientSize = new System.Drawing.Size(800, 281);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.mainPanel);
             this.Controls.Add(this.button2);
@@ -150,6 +189,9 @@ namespace WeatherApp
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
